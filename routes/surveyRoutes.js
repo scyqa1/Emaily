b480948122ed85d16ecbbe3d2f5d47863bfa2c4b
scyqa1 <55672498+scyqa1@users.noreply.github.com>
2022-04-08 15:56:55 -0700

    update credit check function

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,7 +2,7 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, (req, res) => {
-
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+ 
     });
 };
