d1912f1da92cc539c304269fda2011323a4b7db6
scyqa1 <55672498+scyqa1@users.noreply.github.com>
2022-04-09 05:13:04 -0700

    add mailer constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,7 +3,17 @@ const help = sendgrid.mail;
 const keys = require(/../config/keys);
 
 class Mailer extends helper.Mail {
+    constructor({subject, recipients}, content) {
+        super();
+
+        this.from_email = new helper.Email('scyqa27@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+    }
 
 }
+
 
 module.exports = Mailer; 
